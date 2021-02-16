ff75df29f8dcf092225b5a4cb11d2b7d34a90346
luiz binda <luiznandopimenta52@gmail.com>
2021-02-16 16:42:59 -0300

    footer e home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>
-  </div>
+  <section>
+    <h1>👍 Compre ou Venda 👎</h1>
+    <ProdutosBuscar/>
+    <ProdutosLista/>
+  </section>
 </template>
 
 <script>
+import ProdutosBuscar from '@/components/ProdutosBuscar.vue';
+import ProdutosLista from '@/components/ProdutosLista.vue';
 
 export default {
   name: 'Home',
+  components: {
+    ProdutosLista,
+    ProdutosBuscar,
+  },
 };
 </script>
+
+<style scoped>
+h1 {
+  font-size: 2rem;
+  color: #87f;
+  margin-top: 40px;
+  text-align: center;
+}
+
+</style>
